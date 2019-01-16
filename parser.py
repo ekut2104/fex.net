0e33bef1aa56f833c66242ce50698f5674cf2fb9
Melnyk Dmytro <ekut2104@gmail.com>
2019-01-16 17:11:21 +0200

    some upd

--- parser.py
+++ parser.py
@@ -1,23 +1,19 @@
 import requests
 import threading
+import pprint
 
 
 def get_html(url):
     r = requests.get(url)
     if r.json()['result'] == 1:
-        # pprint.pprint(r.json())
-        upload_id = r.json()['upload_list'][0]['upload_id']
-        name = r.json()['upload_list'][0]['name']
-        size = r.json()['folder_upload_size']
-        print(name, upload_id, size, f'https://fex.net/load/254002135070/{upload_id}')
-        return name, upload_id, size, f'https://fex.net/load/254002135070/{upload_id}'
+        print(list(map(lambda x: [x.get('name'), x.get('upload_id'), x.get('size')], r.json()['upload_list'])))
+        return list(map(lambda x: [x.get('name'), x.get('upload_id'), x.get('size')], r.json()['upload_list']))
 
 
 if __name__ == '__main__':
-    token = 200000002030
-    while token < 30000000000:
+    token = 254002135060
+    while token < 254002135092:
         print(token)
         url = f'https://fex.net/j_object_view/{token}'
-        our_thread = threading.Thread(target=get_html(url))
+        get_html(url)
         token += 1
-        our_thread.start()
